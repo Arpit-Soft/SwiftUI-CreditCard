7813ab12a2a80bbaf15e2c96c97f6ec26f9727e8
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-31 18:20:36 +0530

    Initial Commit

---
+++ SwiftUI-CreditCard/SwiftUI_CreditCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CreditCardApp.swift
+//  SwiftUI-CreditCard
+//
+//  Created by Arpit Dixit on 31/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CreditCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
